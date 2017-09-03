2d9f077849a08e7063db58a50185296b4641a712
pnklein <philip_klein@brown.edu>
2017-09-03 17:46:14 -0400

    comment with output format

--- print_out_solution.hpp
+++ print_out_solution.hpp
@@ -3,5 +3,18 @@
 #include <vector>
 #include "assignment.hpp"
 
+/* Output format:
+    <num centers> <num clients>
+    <center x> <center y> <center z>
+    <center x> <center y> <center z>
+    .
+    .
+    <center x> <center y> <center z>
+    <client x> <client y> <center id> <subpopulation> <center id> <subpopulation> ... <center id> <subpopulation>
+    <client x> <client y> <center id> <subpopulation> <center id> <subpopulation> ... <center id> <subpopulation>
+    .
+    .
+    <client x> <client y> <center id> <subpopulation> <center id> <subpopulation> ... <center id> <subpopulation>
+ */
 void print_out(const std::vector<Point> centers, const std::vector<double> &weights, const std::vector<Point> &clients,
                const Assignment &assignment);
